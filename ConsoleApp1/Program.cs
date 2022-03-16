d9efc5f7d9898201dbc6aa8a35f6c89c9f0336c2
Anhtuan Dang <anhtuan91@gmail.com>
2022-03-15 23:42:21 -0400

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+String currentFolder = "D:\\TEMP\\FTP";
+var fileName = "test.file.txt";
+var newExtention = "tmp";
+FileInfo f = new FileInfo(currentFolder + "\\" + fileName);
+if(f.Exists)
+{
+    var newFileName = fileName.Substring(0, fileName.Length - f.Extension.Length) + "." + newExtention;
+    f.MoveTo(currentFolder + "\\" + newFileName);
+}
+
+
+Console.ReadLine();
